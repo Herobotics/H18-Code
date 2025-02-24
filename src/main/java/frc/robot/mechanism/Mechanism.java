c377a69398ce36ba96208b38f42415ba7a301818
Zero2Hero <herobotics2500@gmail.com>
2025-02-24 16:57:33 -0600

    Movement Speed

--- src/main/java/frc/robot/mechanism/Mechanism.java
+++ src/main/java/frc/robot/mechanism/Mechanism.java
@@ -1,5 +1,12 @@
 package frc.robot.mechanism;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
 public class Mechanism {
-    
+
+// intake window motors
+    private final Spark leftIntakeMotor = new Spark(0);
+
+    leftIntakeMotor.set(1.0);
+
 }
