c085515d93d0df2b996556ba38328f81b4c634b8
Zero2Hero <herobotics2500@gmail.com>
2025-03-04 15:42:54 -0600

    add autonomous

--- src/main/java/frc/robot/autonomous/Autonomous.java
+++ src/main/java/frc/robot/autonomous/Autonomous.java
@@ -1,5 +1,17 @@
-package frc.robot.autonomous;
+package frc.robot.autonomous
+;
+public MovementSystem class  {
+    private MovementSystem movement;
+    private SensorSystem sensors;
+    private boolean isRunning;
+    private double straightSpeed =
+1.0; // Default Speed
+    private double correctionFactor =
+0.1; // Correction amount
+
+    public
+StraightLineBot (MovementSystem
+movement, SensorSystem sensors) {}
+}
 
 public class Autonomous {
-    
-}
